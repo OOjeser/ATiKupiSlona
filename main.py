3866a5d54e92dd7d8c65128b292c113db3f015fc
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-04-06 19:50:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,16 @@
-from flask import Flask, request
-import logging
-import json
+import os
+
+from flask import Flask
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
 
-sessionStorage = {}
-
-
-@app.route('/post', methods=['POST'])
-def main():
-    print(request.json)
-    logging.info(f'Request: {request.json!r}')
-    response = {
-        'version': request.json['version'],
-        'session': request.json['session'],
-        'response': {
-            'end_session': False
-        }
-    }
-    handle_dialog(request.json, response)
-    logging.info(f'Response:  {response!r}')
-    return json.dumps(response)
-
-
-def handle_dialog(req, res):
-    user_id = req['session']['user_id']
-
-    if req['session']['new']:
-        sessionStorage[user_id] = {
-            'suggests': [
-                "Не хочу.",
-                "Отстань!",
-                "Нет.",
-            ]
-        }
-        res['response']['text'] = 'Привет! Купи слона!'
-        res['response']['buttons'] = get_suggests(user_id)
-        return
-    if req['request']['original_utterance'].lower() in [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо'
-    ]:
-        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
-        res['response']['end_session'] = True
-        return
-
-    res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
-    res['response']['buttons'] = get_suggests(user_id)
-
-
-def get_suggests(user_id):
-    session = sessionStorage[user_id]
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:2]
-    ]
-    session['suggests'] = session['suggests'][1:]
-    sessionStorage[user_id] = session
-    if len(suggests) < 2:
-        suggests.append({
-            "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон",
-            "hide": True
-        })
-
-    return suggests
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
